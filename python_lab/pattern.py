4ed2533dfc719b3df47a15cd36294d6d585dd23f
Ankita Tandon <ankitatandon227@gmail.com>
2020-11-29 14:48:13 +0530

    Add files via upload

--- python_lab/pattern.py
+++ python_lab/pattern.py
@@ -1,23 +1,13 @@
-'''
-Q2: Write a program to generate the following pattern, using nested for loop.
-
-*
-* *
-* * *
-* * * *
-* * * * *
-* * * *
-* * *
-* *
-*
-
-'''
-
-for i in range(1,6):
-	for j in range(i):
-		print(" *",end="")
-	print("\n",end='')
-for i in range(4,0,-1):
-	for j in range(i):
-		print(" *",end="")
-	print("\n",end='')
+for i in range(4):
+    j=0
+    while j!=i:
+        print(" ",end="")
+        j+=1
+    print("*",end=" ")
+    if i==3:
+        break
+    elif i==0: 
+        print("    ",end="")
+    elif i==1:
+        print("  ",end="")
+    print("*\n")
